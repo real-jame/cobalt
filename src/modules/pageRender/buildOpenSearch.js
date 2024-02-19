0e810856e7a8d3432bb0a7ccddb38435e56c182e
synzr <mikhail@synzr.space>
2024-02-19 13:51:07 +0500

    fix(opensearch): move from OpenSearch-related locstrings to general

--- src/modules/pageRender/buildOpenSearch.js
+++ src/modules/pageRender/buildOpenSearch.js
@@ -8,14 +8,14 @@ export default function (lang) {
     try {
         return `
             <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-                <ShortName>${t('OpenSearchTitle')}</ShortName>
-                <Description>${t("OpenSearchDescription")}</Description>
+                <ShortName>${t('AppTitleCobalt')}</ShortName>
+                <Description>${t("AboutSummary")}</Description>
                 <Url type="text/html" method="get" template="${process.env.webURL}?opensearchquery={searchTerms}" />
                 <Image height="48" width="48" type="image/x-icon">${process.env.webURL}icons/favicon.ico</Image>
                 <Tags>cobalt download</Tags>
             </OpenSearchDescription>
         `
     } catch (err) {
-        return `${t('ErrorOpenSearchBuildFail', lang)}`;
+        return '<!-- failed to generate OpenSearch xml -->';
     }
 }
